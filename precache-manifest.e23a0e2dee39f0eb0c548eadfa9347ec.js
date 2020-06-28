8e1c23b81762647bc2217871d89291f9133aef06
okurutboy <okurut17@gmail.com>
2020-06-28 13:33:41 +0400

    Add files via upload

---
+++ precache-manifest.e23a0e2dee39f0eb0c548eadfa9347ec.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31118f43f32ee76fb9c55018a44c7b2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27a0f1eeb2de3d1c5d5f",
+    "url": "/static/css/2.30c2d978.chunk.css"
+  },
+  {
+    "revision": "13ddcef9c1136d8546c6",
+    "url": "/static/css/main.2b8f5542.chunk.css"
+  },
+  {
+    "revision": "27a0f1eeb2de3d1c5d5f",
+    "url": "/static/js/2.40679290.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/static/js/2.40679290.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13ddcef9c1136d8546c6",
+    "url": "/static/js/main.d6702555.chunk.js"
+  },
+  {
+    "revision": "ac22724fb6bfebf27de6",
+    "url": "/static/js/runtime-main.b2d95604.js"
+  },
+  {
+    "revision": "544818abdec5281e79b5e56b5f4a48b1",
+    "url": "/static/media/newwall.544818ab.png"
+  },
+  {
+    "revision": "6df49e4b4d380649bcf4f5fb313558bd",
+    "url": "/static/media/tick.6df49e4b.mp3"
+  },
+  {
+    "revision": "eb69c905c327d591a01106e86e35cf79",
+    "url": "/static/media/win.eb69c905.mp3"
+  }
+]);
